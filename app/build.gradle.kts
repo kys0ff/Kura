f669c6d275d97fa671e709f9bd2b25e14861d782
kys0ff <idk683120@gmail.com>
2026-03-21 14:40:14 +0100

    Update version name to 1.0.0 and enable minification for release builds.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,14 +16,14 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
