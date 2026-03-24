d8ea83f629c961bf429d0797c2ac1b0bc870810b
kys0ff <idk683120@gmail.com>
2026-03-24 22:42:01 +0100

    Update app version to 1.0.2 and increment version code.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "off.kys.kura"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
