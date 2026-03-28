f1b7a58056d29a0c761ffa135001d87dcffb5637
kys0ff <idk683120@gmail.com>
2026-03-28 11:23:16 +0100

    Configure status bar appearance to adjust based on theme selection.

--- app/src/main/java/off/kys/kura/core/designsystem/theme/Theme.kt
+++ app/src/main/java/off/kys/kura/core/designsystem/theme/Theme.kt
@@ -1,6 +1,7 @@
 package off.kys.kura.core.designsystem.theme
 
 import android.os.Build
+import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -8,7 +9,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = darkPrimary,
@@ -75,6 +79,16 @@ fun KuraTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    val window = LocalActivity.current?.window
+    if (!view.isInEditMode) {
+        SideEffect {
+            if (window == null)
+                return@SideEffect
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     MaterialTheme(
