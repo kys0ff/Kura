9c63ec7fac9e4951e2cb7011a0fcb020e8eb502c
kys0ff <idk683120@gmail.com>
2026-04-09 17:37:55 +0100

    Update logic in LockerAccessibilityService to treat a negative lock timeout as an infinite grace period.

--- app/src/main/java/off/kys/kura/features/lock/service/LockerAccessibilityService.kt
+++ app/src/main/java/off/kys/kura/features/lock/service/LockerAccessibilityService.kt
@@ -96,8 +96,7 @@ class LockerAccessibilityService : AccessibilityService() {
 
         // 2. The "User was just here" Check
         val timeSinceLastInteraction = currentTime - lastUnlockTime
-        val isWithinGracePeriod =
-            appPrefs.lockTimeout != -1L && timeSinceLastInteraction < appPrefs.lockTimeout
+        val isWithinGracePeriod = appPrefs.lockTimeout == -1L || timeSinceLastInteraction < appPrefs.lockTimeout
 
         if (registry.isPackageLocked(currentPackage)) {
             if (currentPackage == lastUnlockedPackage && isWithinGracePeriod) {
