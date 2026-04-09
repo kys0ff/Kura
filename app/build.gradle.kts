9814731ad1f2cf620156b2f575945df01eb8d13e
kys0ff <idk683120@gmail.com>
2026-04-09 17:06:48 +0100

    Update application version code to 3 and version name to 1.0.3.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "off.kys.kura"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
